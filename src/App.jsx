ba78704712cc98ea9c3b7711473efa2264e83826
Héctor <hartisca91@gmail.com>
2024-12-03 22:18:42 +0100

    Prueba realizada

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,41 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
   const [ fact, setFact ] = useState('')
+  const [ img, setImg ] = useState('')
   
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
-      .then((response) => response.json())
-        .then((data) => setFact(data.fact))
-  },[])
+    const fetchData = async () => {
+      await fetchFact()
+      await fetchImg()
+    }
+    fetchData()
+  }, [])
+
+  const fetchFact = async () => {
+    try{
+      const respuesta = await fetch('https://catfact.ninja/fact')
+      const data = await respuesta.json()
+      const fact2 = data.fact.split(" ")
+      setFact(fact2[0])
+    } catch (error){
+      console.log(error)
+    }      
+  }
+
+  const fetchImg = async () => {
+    try{
+      const respuesta = await fetch('https://cataas.com/cat/says/hello')      
+      setImg(respuesta.url)      
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <>
-      <h1>Cosas de gatos: {fact}</h1>
+      <h1>{fact}</h1>
+      <img src={img} alt="" />
     </>
   )
 }
